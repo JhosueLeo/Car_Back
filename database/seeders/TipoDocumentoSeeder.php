6995c35a7ebca3262004be735b87cc3f87a3decb
JhosueLeo <150406643+JhosueLeo@users.noreply.github.com>
2024-05-09 19:34:11 -0500

    SeederTipoDocumento

---
+++ database/seeders/TipoDocumentoSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\TipoDocumento;
+
+class TipoDocumentoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TipoDocumento::firstOrcreate([
+            'nombre'=>'DNI',
+            'descripcion'=>'Documento Nacional de Identidad'
+        ]);
+        TipoDocumento::firstOrCreate([
+            'nombre'=>'RUC',
+            'descripcion'=>'Registro Unico Contribuyente'
+        ]);
+    }
+}
