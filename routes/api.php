7f0b1fcb73bd75b6cfa6164ff5f1a14ea899881f
JhosueLeo <150406643+JhosueLeo@users.noreply.github.com>
2024-05-17 20:43:50 -0500

    Agrupacion de Rutas

--- routes/api.php
+++ routes/api.php
@@ -13,40 +13,53 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 //Categoria
+Route::group(['prefix' => 'categoria'], function () {
+    Route::get('/get',[CategoriaController::class,'get']);
+    Route::get('/show/{idCategoria}',[CategoriaController::class,'show']);
+    Route::post('/create',[CategoriaController::class,'create']);
+    Route::post('/update/{idCategoria}',[CategoriaController::class,'update']);
+    Route::post('/delete/{idCategoria}',[CategoriaController::class,'delete']);
+});
 Route::post('login', [AuthController::class, 'authenticate']);
-Route::get('/categoria/get',[CategoriaController::class,'get']);
-Route::get('/categoria/show/{idCategoria}',[CategoriaController::class,'show']);
-Route::post('/categoria/create',[CategoriaController::class,'create']);
-Route::post('/categoria/update/{idCategoria}',[CategoriaController::class,'update']);
-Route::post('/categoria/delete/{idCategoria}',[CategoriaController::class,'delete']);
+
 //Producto
-Route::get('/producto/get',[ProductoController::class,'get']);
-Route::get('/producto/show/{idProducto}',[ProductoController::class,'show']);
-Route::post('/producto/create',[ProductoController::class,'create']);
-Route::post('/producto/update/{idProducto}',[ProductoController::class,'update']);
-Route::post('/producto/delete/{idProducto}',[ProductoController::class,'delete']);
+Route::group(['prefix' => 'producto'], function () {
+    Route::get('/get',[ProductoController::class,'get']);
+    Route::get('/show/{idProducto}',[ProductoController::class,'show']);
+    Route::post('/create',[ProductoController::class,'create']);
+    Route::post('/update/{idProducto}',[ProductoController::class,'update']);
+    Route::post('/delete/{idProducto}',[ProductoController::class,'delete']);
+});
 //Inventario
-Route::get('/inventario/get',[InventarioController::class,'get']);
-Route::get('/inventario/show/{idInventario}',[InventarioController::class,'show']);
-Route::post('/inventario/create',[InventarioController::class,'create']);
-Route::post('/inventario/update/{idInventario}',[InventarioController::class,'update']);
-Route::post('/inventario/delete/{idInventario}',[InventarioController::class,'delete']);
+Route::group(['prefix' => 'inventario'], function () {
+    Route::get('/get',[InventarioController::class,'get']);
+    Route::get('/show/{idInventario}',[InventarioController::class,'show']);
+    Route::post('/create',[InventarioController::class,'create']);
+    Route::post('/update/{idInventario}',[InventarioController::class,'update']);
+    Route::post('/delete/{idInventario}',[InventarioController::class,'delete']);
+});
 //Servicio
-Route::get('/servicio/get',[ServicioController::class,'get']);
-Route::get('/servicio/show/{idInventario}',[ServicioController::class,'show']);
-Route::post('/servicio/create',[ServicioController::class,'create']);
-Route::post('/servicio/update/{idInventario}',[ServicioController::class,'update']);
-Route::post('/servicio/delete/{idInventario}',[ServicioController::class,'delete']);
+Route::group(['prefix' => 'servicio'], function () {
+    Route::get('/get',[ServicioController::class,'get']);
+    Route::get('/show/{idInventario}',[ServicioController::class,'show']);
+    Route::post('/create',[ServicioController::class,'create']);
+    Route::post('/update/{idInventario}',[ServicioController::class,'update']);
+    Route::post('/delete/{idInventario}',[ServicioController::class,'delete']);
+});
 //Cliente
-Route::get('/cliente/get',[ClienteController::class,'get']);
-Route::get('/cliente/show/{idCliente}',[ClienteController::class,'show']);
-Route::post('/cliente/create',[ClienteController::class,'create']);
-Route::post('/cliente/update/{idCliente}',[ClienteController::class,'update']);
-Route::post('/cliente/delete/{idCliente}',[ClienteController::class,'delete']);
+Route::group(['prefix' => 'cliente'], function () {
+    Route::get('/get',[ClienteController::class,'get']);
+    Route::get('/show/{idCliente}',[ClienteController::class,'show']);
+    Route::post('/create',[ClienteController::class,'create']);
+    Route::post('/update/{idCliente}',[ClienteController::class,'update']);
+    Route::post('/delete/{idCliente}',[ClienteController::class,'delete']);
+});
 //Empleado
-Route::get('/empleado/get',[EmpleadoController::class,'get']);
-Route::get('/empleado/show/{idCliente}',[EmpleadoController::class,'show']);
-Route::post('/empleado/create',[EmpleadoController::class,'create']);
-Route::post('/empleado/update/{idCliente}',[EmpleadoController::class,'update']);
-Route::post('/empleado/delete/{idCliente}',[EmpleadoController::class,'delete']);
-Route::post('/empleado/asignar_rol/{idCliente}',[EmpleadoController::class,'asignarRol']);
+Route::group(['prefix' => 'empleado'], function () {
+    Route::get('/get',[EmpleadoController::class,'get']);
+    Route::get('/show/{idCliente}',[EmpleadoController::class,'show']);
+    Route::post('/create',[EmpleadoController::class,'create']);
+    Route::post('/update/{idCliente}',[EmpleadoController::class,'update']);
+    Route::post('/delete/{idCliente}',[EmpleadoController::class,'delete']);
+    Route::post('/asignar_rol/{idCliente}',[EmpleadoController::class,'asignarRol']);
+});
